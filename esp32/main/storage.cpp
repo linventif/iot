e93b1af9cef85e8bf2efdca80aa7a07f77c66d2f
linventif <gregoire.becue@proton.me>
2025-07-26 18:41:42 +0200

    Fix temperature threshold antiback loading and saving in config

--- esp32/main/storage.cpp
+++ esp32/main/storage.cpp
@@ -10,12 +10,13 @@ void initStorage() {
 bool loadConfig(AppConfig &cfg) {
   cfg.deviceId      = prefs.getString(CFG_KEY_DEVICE_ID, DEFAULT_DEVICE_ID);
   cfg.tempThreshold = prefs.getFloat(CFG_KEY_TEMP_THRESHOLD, DEFAULT_TEMP_THRESHOLD);
-  cfg.tempThresholdAntiback = prefs.getFloat(DEFAULT_TEMP_THRESHOLD_ANTIBACK);
+  cfg.tempThresholdAntiback = prefs.getFloat(CFG_KEY_TEMP_THRESHOLD_ANTIBACK, DEFAULT_TEMP_THRESHOLD_ANTIBACK);
   return true;
 }
 
 bool saveConfig(const AppConfig &cfg) {
   prefs.putString(CFG_KEY_DEVICE_ID, cfg.deviceId);
   prefs.putFloat(CFG_KEY_TEMP_THRESHOLD, cfg.tempThreshold);
+  prefs.putFloat(CFG_KEY_TEMP_THRESHOLD_ANTIBACK, cfg.tempThresholdAntiback);
   return true;
 }
