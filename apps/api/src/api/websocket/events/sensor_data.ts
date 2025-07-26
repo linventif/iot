9190ce82750d7630b99da2cadc019c64124a0a4b
linventif <gregoire.becue@proton.me>
2025-07-26 15:57:01 +0200

    refactor: update WebSocketEvent to use ElysiaWS type for wsClient

--- apps/api/src/api/websocket/events/sensor_data.ts
+++ apps/api/src/api/websocket/events/sensor_data.ts
@@ -1,9 +1,10 @@
 import { SensorData } from '../../../classes/SensorData';
 import { SensorDataWebSocketType } from '@schemas/src/SensorData';
 import { WebSocketEvent } from '../../../classes/WebSocketEvent';
+import { ElysiaWS } from 'elysia/dist/ws';
 
 export default new WebSocketEvent()
 	.setType('sensor_data')
-	.setOnData(async (wsClient: any, data: SensorDataWebSocketType) => {
+	.setOnData(async (wsClient: ElysiaWS, data: SensorDataWebSocketType) => {
 		SensorData.fromInput(data).save();
 	});
