69ba8a53b6af80aa3bd8646fbb82329d9b769484
linventif <gregoire.becue@proton.me>
2025-09-09 14:26:12 +0200

    fix: forgot the /api

--- apps/website/src/utils/utils.ts
+++ apps/website/src/utils/utils.ts
@@ -2,5 +2,5 @@ export function getAPIUrl(): string {
 	if (window.location.hostname === 'localhost') {
 		return 'http://localhost:4001';
 	}
-	return `https://iot-api.linv.dev`;
+	return `https://iot-api.linv.dev/api`;
 }
